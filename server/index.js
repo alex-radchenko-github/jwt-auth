b0239cfd64c8b55a9def0fe330be7f5e141321f9
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2023-01-10 15:19:57 -0800

    add servises

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
+const {MongoClient, ServerApiVersion} = require('mongodb');
+const router = require('./router/index.js');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -10,14 +11,16 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors())
+app.use('/api', router);
 
 
 const start = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        const client = new MongoClient(process.env.DB_URL, {
+            useNewUrlParser: true, useUnifiedTopology: true
+        });
+        await client.connect();
+
         app.listen(process.env.PORT, () => {
             console.log(`Server has been started on ${PORT}`);
         })
---
+++ server/service/user-service.js
@@ -0,0 +1,20 @@
+const UserModels = require('../models/user-model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail-service');
+
+class UserService {
+    async registration(email, password) {
+        const candidate = await UserModels.findOne({email});
+        if (candidate) {
+            throw new Error('User with this email already exists');
+        }
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4();
+        const user = await UserModels.create({email, password: hashPassword, activationLink});
+        await mailService.sendActivationMail(email, activationLink);
+    }
+
+}
+
+module.exports = new UserService();
