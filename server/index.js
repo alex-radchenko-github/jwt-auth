c2b2b69958f73f2f538e7bddd9febda710159b5a
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2023-01-12 15:09:29 -0800

    add logout

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const router = require('./router/index.js');
-const mongoose = require('mongoose');
+const {connect} = require('mongoose');
 const errorMiddleware = require('./middlewares/error-middleware.js');
 
 
@@ -20,7 +20,7 @@ app.use(errorMiddleware);
 const start = async () => {
     try {
 
-        await mongoose.connect(process.env.DB_URL, {
+        await connect(process.env.DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -53,6 +53,10 @@ class UserService {
 
 
     }
+    async logout(refreshToken) {
+        const token = await tokenService.removeToken(refreshToken);
+        return token;
+    }
 
 }
 
